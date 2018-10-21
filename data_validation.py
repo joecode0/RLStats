3a02b7edc964fcf5b8b1b7589dc66fb02d829946
joecode <joecode@zoho.eu>
2018-10-21 15:01:56 +0100

    Validated and fixed team names

--- data_validation.py
+++ data_validation.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
 
+def check_team_names_consistent(filename):
+    df = pd.read_csv(filename)
+    teamname_set = sorted(set(df['Team'].tolist()))
+    print(teamname_set)
+
+    original_col_order = list(df.columns.values)
+    df['Team'] = df.apply(fix_team_names, axis=1)
+    teamname_set = sorted(set(df['Team'].tolist()))
+    print(teamname_set)
+    df = df[original_col_order]
+    df.to_csv(filename, index=False)
+
+
 def check_player_names_consistent(filename):
     # By printing out a set of players SORTED, can easily see if players have the wrong name in some places
     df = pd.read_csv(filename)
@@ -10,8 +23,34 @@ def check_player_names_consistent(filename):
     # This pointed out that the names "KUXIR" and "KUXIR97" has been used interchangeably, and therefore would cause plotting bugs. Fix below
     original_col_order = list(df.columns.values)
     df['Player'] = df.apply(fix_player_names, axis=1)
+    players_set = sorted(set(df['Player'].tolist()))
+    print(players_set)
     df = df[original_col_order]
     df.to_csv(filename, index=False)
+
+
+def fix_team_names(row):
+    team = row['Team']
+    if team == 'MOCK-IT EU ':
+        return "MOCK-IT EU"
+    elif team == "COMPLEXITY GAMING":
+        return "COMPLEXITY"
+    elif team == "TEAM DIGNITAS":
+        return "DIGNITAS"
+    elif team == "TEAM ENVYUS":
+        return "ENVYUS"
+    elif team == "TEAM VITALITY":
+        return "RENAULT VITALITY"
+    elif team == "NRG ESPORTS":
+        return "NRG"
+    elif team == "PSG ESPORTS":
+        return "PSG"
+    elif team == "SELFLESS GAMING":
+        return "SELFLESS"
+    elif team == "THE LEFTOVERS":
+        return "LEFTOVERS"
+    else:
+        return team
 
 
 def fix_player_names(row):
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import data_validation
 
 player_stats = "RLCS_Data/LAN_player_stats.csv"
 if __name__ == '__main__':
-    data_validation.check_player_names_consistent(player_stats)
+    data_validation.check_team_names_consistent(player_stats)
