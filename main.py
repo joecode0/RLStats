27b22b5eee761e131d73e07d33570f9cda123fce
joecode <joecode@zoho.eu>
2018-10-22 14:02:48 +0100

    Added code to rank players by LAN finishes

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import data_validation
 
 player_stats = "RLCS_Data/LAN_player_stats.csv"
 if __name__ == '__main__':
-    player_positions.plot_best_LAN_finishers(player_stats, 4)
+    f = player_positions.calculate_score_per_player(player_stats)
+    print(f)
--- player_positions.py
+++ player_positions.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+SEASON_TEAMS_DICT = {"RLCS1": 8, "RLCS2": 8,
+                     "RLCS3": 10, "RLCS4": 10, "RLCS5": 10}
+
 
 def plot_best_LAN_finishers(filename, reqNoOfLANS):
     best_players_dict = get_best_LAN_players(filename, reqNoOfLANS)
@@ -14,6 +17,27 @@ def get_best_LAN_players(filename, reqNoOfLANS):
         if len(position_dict.keys()) >= reqNoOfLANS:
             best_players_dict[player] = position_dict
     return best_players_dict
+
+
+def calculate_score_per_player(filename):
+    positions_dict = get_player_LAN_positions_dict(filename)
+    scores_dict = {}
+    for player in positions_dict.keys():
+        score = 0
+        finishes_dict = positions_dict.get(player)
+        for i in range(1, 6):
+            key = "RLCS" + str(i)
+            if key in finishes_dict.keys():
+                score += finishes_dict[key]
+            else:
+                score += (SEASON_TEAMS_DICT[key] + 1)
+        scores_dict[player] = score
+    df = pd.DataFrame([], columns=['Player', 'Score'])
+    df['Player'] = scores_dict.keys()
+    df['Score'] = scores_dict.values()
+    df.sort_values(by=['Score', 'Player'], axis=0,
+                   ascending=True, inplace=True)
+    return df
 
 
 def get_player_LAN_positions_dict(filename):
